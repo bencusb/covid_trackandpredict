723354b57b9ea87b0c83cb57f932ce70000c5ef3
Yama <balintmlnr@gmail.com>
2021-11-12 13:57:36 +0100

    a

---
+++ CovidGorbe/src/jUnit/basicMath2.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jUnit;
+
+/**
+ * For jUnit Test
+ * @author Balint
+ */
+public class basicMath2 {
+    /**
+     * subtracts 2 values
+     * 
+     * @param a a number
+     * @param b a number
+     * @return the subtraction of 2 numbers
+     */
+    public double kivonas(double a, double b){
+        return a - b;
+    }
+
+    /**
+     * divides 2 values
+     * 
+     * @param a a number
+     * @param b a number
+     * @return the division of the 2 numbers
+     */
+    public double osztas(double a, double b){
+        return a / b;
+    }
+
+    /**
+     * Returns the squared value
+     * 
+     * @param a a number
+     * @return the squared value
+     */
+    public double square(double a){
+        return a*a;
+    }
+}
